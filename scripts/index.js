592cf1505b49982d1665d4f7aa954887f4743a5f
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-04-15 00:59:28 +0100

    add delete and like buttons on cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,18 @@
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
-const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_place_profile');
 const buttonPen = document.querySelector('.profile__button-pen');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const popupClose = document.querySelector('.popup .popup__close');
+const popupClose = document.querySelectorAll('.popup .popup__close');
 const popupForm = document.querySelector('.popup__inputs');
+
+const popupAddCard = document.querySelector('.popup_place_add-card');
+const buttonAdd = document.querySelector('.profile__button-add');
+
+
+const popupFormAdd = document.querySelector('.popup__inputs_type_add');
+
 
 const initialCards = [
     {
@@ -48,34 +55,87 @@ function getElement(item) {
   elementListTitle.textContent = item.name;
   const elementListImage = getElementTemplate.querySelector('.elements__list-image');
   elementListImage.src = item.link;
+  const removeButton = getElementTemplate.querySelector('.elements__remove');
+  removeButton.addEventListener('click', removeElement);
+  const likeButton = getElementTemplate.querySelector('.elements__list-button');
+  likeButton.addEventListener('click', likeElement);
   return getElementTemplate;
 
 }
 
 render();
 
+function likeElement(evt) {
+  console.log(evt);
+  const element = evt.target;
+  element.classList.add('elements__list-button_active');
+  element.classList.remove('element__list-button_inactive');
+}
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function removeElement(evt) {
+  const element = evt.target.closest('.elements__list-item');
+  element.remove();
+  
+}
+
+function closePopup(evt) {
+  evt.target.parentNode.parentNode.classList.remove('popup_opened');
 }
 
 buttonPen.addEventListener('click', function() {
     inputName.value = profileName.innerText;
     inputDescription.value = profileDescription.innerText;
-    popup.classList.add('popup_opened');
+    popupProfile.classList.add('popup_opened');
 });
 
-popupClose.addEventListener('click', function() {
+buttonAdd.addEventListener('click', function() {
+    
+    popupAddCard.classList.add('popup_opened');
+    console.log('button add clicked');
+
+});
+
+
+for (let i = 0; i < popupClose.length; i++) {
+  popupClose[i].addEventListener('click', function(evt) {
     console.log('closing popup');
-    closePopup();
+    closePopup(evt);
     inputName.value = '';
     inputDescription.value = ''; 
-});
+  });
+}
+
+// popupClose.addEventListener('click', function(evt) {
+//     console.log('closing popup');
+//     closePopup(evt);
+//     inputName.value = '';
+//     inputDescription.value = ''; 
+// });
+
+// for (let i = 0; i < popupForm.length; i++) {
+//     popupForm[i].addEventListener('submit', function(evt) {
+//         console.log('submitting form');
+//         evt.preventDefault();
+//         profileName.innerText = inputName.value;
+//         profileDescription.innerText = inputDescription.value;
+//         closePopup(evt);
+//     });
+//   }
 
 popupForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
     profileName.innerText = inputName.value;
     profileDescription.innerText = inputDescription.value;
-    closePopup();
+    closePopup(evt);
 });
 
+
+popupFormAdd.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  const inputTitle = document.querySelector('.popup__input_type_title').value;
+  const inputLink = document.querySelector('.popup__input_type_link').value;
+  const element = getElement({name: inputTitle, link: inputLink});
+  elementsList.prepend(element);
+  closePopup(evt);
+  
+});
