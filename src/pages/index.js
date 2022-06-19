47656aa33059abeddaa883714e33af3279e56080
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-06-19 23:39:25 +0100

    Likes work

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,31 +16,142 @@ import { FormValidator } from "../../components/FormValidator.js";
 import PopupWithImage from "../../components/PopupWithImage.js";
 import UserInfo from "../../components/UserInfo.js";
 import PopupWithForm from "../../components/PopupWithForm.js";
+import Api from "../../components/Api.js";
+import PopupWithSubmit from "../../components/PopupWithSubmit.js";
 
 const popupWithImage = new PopupWithImage(".popup_place_show-card");
+
+const api = new Api(
+  "https://mesto.nomoreparties.co/v1/cohort-43/cards",
+  "c1d97d50-899b-43cb-b95d-18fefc90a34b"
+);
+
+let userId = undefined;
 
 function createCard(data) {
   const card = new Card(
     {
       data: data,
+      userId: userId,
       handleCardClick: () => {
         popupWithImage.open(data.link, data.name);
+      },
+      handleRemoveCard: (card) => {
+        deleteCardHandler(card);
+      },
+      handleLikeClick: (cardId, isLike) => {
+        return likeCardHandler(cardId, isLike);
       },
     },
     ".template-elements"
   );
+
   return card.generateCard();
 }
 
 const cardsSection = new Section(
   {
-    items: initialCards,
+    items: [],
     renderer: createCard,
   },
   ".elements__list"
 );
-
 cardsSection.renderItems();
+
+/*
+api
+  .getInitialCards()
+  .then((cards) => {
+    cards
+      .slice()
+      .reverse()
+      .forEach((card) => {
+        cardsSection.addItem(card);
+        if (card.owner._id !== myId) {
+          cardsSection._container
+            .querySelector(".elements__remove")
+            .classList.add("elements__remove_hidden");
+        }
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+*/
+
+api
+  .getUserInfo()
+  .then((res) => {
+    console.log(res);
+    userId = res._id;
+  })
+  .then(() => api.getInitialCards())
+  .then((cards) => {
+    cards
+      .slice()
+      .reverse()
+      .forEach((card) => {
+        //
+        cardsSection.addItem(card);
+        /*
+        if (card.owner._id !== results[1]) {
+          cardsSection._container
+            .querySelector(".elements__remove")
+            .classList.add("elements__remove_hidden");
+        }
+        let likesCount = card.likes.length;
+        cardsSection._container.querySelector(
+          ".elements__like-count"
+        ).textContent = likesCount;
+        */
+      });
+  })
+  .catch((err) => console.log(err));
+
+// api
+//   .updateUserInfo()
+//   .then((res) => {
+//     console.log(`result of updateUserInfo ${res}`);
+//   })
+//   .catch((err) => {
+//     console.log(`Error of udateUserInfo ${err}`);
+//   });
+
+function callbackOnDelete() {
+  //card.removeCard();
+}
+
+function deletePopupSubmitHandler(card) {
+  api
+    .deleteCard(card.getId())
+    .then((res) => {
+      card.removeCard();
+    })
+    .catch((err) => console.log(err));
+}
+
+function deleteCardHandler(card) {
+  const popupWithSubmit = new PopupWithSubmit(".popup__confirm", () =>
+    deletePopupSubmitHandler(card)
+  );
+  popupWithSubmit.open();
+  popupWithSubmit.setEventListeners();
+}
+
+function likeCardHandler(cardId, isLike) {
+  if (isLike) {
+    return api.putLike(cardId).then((res) => {
+      //console.log(`${card.getLikes()} card liked by`);
+      //console.log(`this is result ${res}`);
+      return res.likes;
+    });
+  } else {
+    return api.deleteLike(cardId).then((res) => {
+      console.log("like removed");
+      return res.likes;
+    });
+  }
+}
 
 const userInfo = new UserInfo(".profile__name", ".profile__description");
 
@@ -67,8 +178,12 @@ const popupWithFormEdit = new PopupWithForm(
   callbackEdit
 );
 
+// const popupWithFormAdd = new PopupWithForm(".popup_place_add-card", (data) => {
+//   cardsSection.addItem(data);
+// });
+
 const popupWithFormAdd = new PopupWithForm(".popup_place_add-card", (data) => {
-  cardsSection.addItem(data);
+  api.addCard(data).then((data) => cardsSection.addItem(data));
 });
 
 popupWithFormEdit.setEventListeners();
